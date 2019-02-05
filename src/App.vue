343e56d7fa86daeba07b8291ca5907066cae53f9
markhorsman <mark.horsman@tam-one.nl>
2019-02-05 20:31:59 +0100

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <Slide noOverlay right>
-      <router-link to="/"><span>Home</span></router-link>
-      <router-link to="/settings"><span>Instellingen</span></router-link>
-      <router-link to="/artikelen/gekeurd"><span>Gekeurde artikelen</span></router-link>
-      <router-link to="/artikelen/ongekeurd"><span>Ongekeurde artikelen</span></router-link>
-      <router-link to="/artikelen/verlopen"><span>Verlopen artikelen</span></router-link>
-    </Slide>
+      <b-navbar toggleable="md" type="light" variant="dark">
+
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+          <b-navbar-brand href="#"><img width="40" src="./assets/logo.png"/></b-navbar-brand>
+
+          <b-collapse is-nav id="nav_collapse">
+
+            <b-navbar-nav>
+                <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
+                <b-nav-item href="#"><router-link to="/settings"><span>Instellingen</span></router-link></b-nav-item>
+                <b-nav-item href="#"><router-link to="/artikelen/gekeurd"><span>Gekeurde artikelen</span></router-link></b-nav-item>
+                <b-nav-item href="#"><router-link to="/artikelen/ongekeurd"><span>Ongekeurde artikelen</span></router-link></b-nav-item>
+                <b-nav-item href="#"><router-link to="/artikelen/verlopen"><span>Verlopen artikelen</span></router-link></b-nav-item>
+            </b-navbar-nav>
+
+          </b-collapse>
+        </b-navbar>
+
     <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
-import { Slide } from 'vue-burger-menu'
 export default {
-  name: 'app',
-  components: {
-     Slide
-  }
+  name: 'app'
 }
 </script>
 
@@ -26,16 +34,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-
-  margin-top: 20px;
 }
-.bm-burger-button {
-  top: 0;
-  z-index: 10;
-}
-/* .bm-burger-bars {
-  background-color: #ffffff;
-} */
-
 </style>
